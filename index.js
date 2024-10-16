d54094c043a613dc03e1dea302796f00bded57c9
valeriaa4 <santosvaleriav4@gmail.com>
2024-10-16 20:58:57 -0300

    classificando os níveis

--- index.js
+++ index.js
@@ -3,3 +3,21 @@
 const nomeHeroi = "Json Scripto"
 let quantidadeXP = 9002
 let nivel = ""
+
+if(quantidadeXP <= 1000){
+    nivel = "Ferro"
+} else if(quantidadeXP >= 1001 && quantidadeXP <= 2000){
+    nivel = "Bronze"
+} else if(quantidadeXP >= 2001 && quantidadeXP <= 5000){
+    nivel = "Prata"
+} else if(quantidadeXP >= 5001 && quantidadeXP <= 7000){
+    nivel = "Ouro"
+} else if(quantidadeXP >= 7001 && quantidadeXP <= 8000){
+    nivel = "Platina"
+} else if(quantidadeXP >= 8001 && quantidadeXP <= 9000){
+    nivel = "Ascendente"
+} else if(quantidadeXP >= 9001 && quantidadeXP <= 10000){
+    nivel = "Imortal"
+} else{
+    nivel = "Radiante"
+}
